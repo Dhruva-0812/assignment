f7560317b5df3a62c9feede59ae12fe961e36e00
Dhruva K R <dhruvadhanush02@gmail.com>
2025-03-30 23:47:16 +0530

    Create task2-assignment-3.py

---
+++ task2-assignment-3.py
@@ -0,0 +1,8 @@
+import math
+a=int(input("eneter a number:"))
+sqrt_value = math.sqrt(a)
+print("square root of " , a , "=" ,sqrt_value)
+log_value = math.log(a) 
+print("Natural logarithm of " , a ," is: " , log_value)
+sin_value= math.sin(a)
+print("Sin of" , a , "=" , sin_value);
