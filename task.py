4e4f83cbbeff6a62b7fc27ab713b7780fa973b40
Dhruva K R <dhruvadhanush02@gmail.com>
2025-03-30 20:08:06 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,1 +1,7 @@
-
+print("Hello")
+a= int(input("enter a number:"))
+b =int(input("enter the second number:"))
+print("addition=" ,a+b)
+print("subtraction=" ,a-b)
+print("multiplication=" ,a*b)
+print("division=" ,a/b)
