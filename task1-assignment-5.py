98125ed6b05b0beee9dde12e821e2fffd2a06d91
Dhruva K R <dhruvadhanush02@gmail.com>
2025-05-29 23:02:04 +0530

    Update task1-assignment-5.py
    new

--- task1-assignment-5.py
+++ task1-assignment-5.py
@@ -1,6 +1,6 @@
 #creation of dictionary named details and keys as the name and marks as the value
-details={"Ganesh":50,"Vignesh":45,"Ramesh":50,"Digvesh":15,"Shana":36}
-search=input("enter the name of the student:")
+details={"GANESH":50,"VIGNESH":45,"RAMESH":50,"DIGVESH":15,"SHANA":36}
+search=input("enter the name of the student(all in uppercase):")
 a=search in details
 if(a==True):
   z=details[search]
